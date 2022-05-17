4496254aa1cc85c0cc893db14fbd631988ef6766
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-17 14:21:49 -0700

    lessons 11, 12, 13 - Dynamic Routes, Fetching a Single Item

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,10 +1,12 @@
 import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link';
+
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
   return {
-    props: { ninjas: data}
+    props: { ninjas: data }
   }
 }
 
@@ -13,11 +15,11 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ ninja.name }</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   )
