56269c765a9cbaab4b05b0c444fe5fddb2056687
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-17 12:27:17 -0700

    lesson 9 - images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Nulla laborum philosophari qui sed malis familiaritatem. In legam tempor dolor consequat a ea cernantur firmissimum, in aliqua reprehenderit, eram ingeniis hic nulla nisi. Velit ea sed aute incididunt, ex anim multos illum mentitum de iudicem anim sint senserit noster in incurreret fore ipsum ea sunt.Velit quamquam illustriora, labore mentitum do commodo, o sint quamquam comprehenderit, a elit vidisse expetendis. Si expetendis sed mandaremus, incididunt sunt hic ingeniis arbitrantur ad excepteur eu velit, cupidatat elit deserunt. Fugiat appellat te exercitation, multos in a anim possumus, e quae multos veniam mentitum id enim nostrud eu magna magna.</p>
-      <p>Nescius aute nisi an labore, offendit veniam irure nescius summis qui malis praetermissum expetendis veniam constias ut quorum ab a dolor constias. Doctrina ubi quid nescius, noster cupidatat familiaritatem. Aut ne enim irure quorum, ab ut transferrem, fore doctrina ita anim ipsum est quem consequat laborum.Id sunt ea quae qui id tamen cernantur relinqueret, multos appellat id tractavissent, ab export graviterque non deserunt multos excepteur voluptate, do sed anim nostrud, duis de an nisi incididunt, velit excepteur incurreret. Et nam fidelissimae, deserunt ab mentitum do arbitror eu dolore, doctrina anim mandaremus admodum. Appellat nisi quamquam possumus. Cupidatat et laborum, tempor qui nam minim fabulas.</p>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Nulla laborum philosophari qui sed malis familiaritatem. In legam tempor dolor consequat a ea cernantur firmissimum, in aliqua reprehenderit, eram ingeniis hic nulla nisi. Velit ea sed aute incididunt, ex anim multos illum mentitum de iudicem anim sint senserit noster in incurreret fore ipsum ea sunt.Velit quamquam illustriora, labore mentitum do commodo, o sint quamquam comprehenderit, a elit vidisse expetendis. Si expetendis sed mandaremus, incididunt sunt hic ingeniis arbitrantur ad excepteur eu velit, cupidatat elit deserunt. Fugiat appellat te exercitation, multos in a anim possumus, e quae multos veniam mentitum id enim nostrud eu magna magna.</p>
+        <p>Nescius aute nisi an labore, offendit veniam irure nescius summis qui malis praetermissum expetendis veniam constias ut quorum ab a dolor constias. Doctrina ubi quid nescius, noster cupidatat familiaritatem. Aut ne enim irure quorum, ab ut transferrem, fore doctrina ita anim ipsum est quem consequat laborum.Id sunt ea quae qui id tamen cernantur relinqueret, multos appellat id tractavissent, ab export graviterque non deserunt multos excepteur voluptate, do sed anim nostrud, duis de an nisi incididunt, velit excepteur incurreret. Et nam fidelissimae, deserunt ab mentitum do arbitror eu dolore, doctrina anim mandaremus admodum. Appellat nisi quamquam possumus. Cupidatat et laborum, tempor qui nam minim fabulas.</p>
     </div>
+    </>
   )
 }
 
